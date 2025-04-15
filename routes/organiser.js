15dd7d7f8414a26f384f24d3725e1c2b7f590222
Joe <30784663+joem04@users.noreply.github.com>
2025-04-15 17:44:40 +0100

    Add course edit and participant list route to organiser panel

--- routes/organiser.js
+++ routes/organiser.js
@@ -1,4 +1,3 @@
-// routes/organiser.js
 const express = require('express');
 const router = express.Router();
 const coursesDB = require('../models/courses');
@@ -38,6 +37,34 @@ router.post('/organiser/courses/new', ensureOrganiser, (req, res) => {
 router.post('/organiser/courses/:id/delete', ensureOrganiser, (req, res) => {
   coursesDB.remove({ _id: req.params.id }, {}, () => {
     res.redirect('/organiser/dashboard');
+  });
+});
+
+// Edit form
+router.get('/organiser/courses/:id/edit', ensureOrganiser, (req, res) => {
+  coursesDB.findOne({ _id: req.params.id }, (err, course) => {
+    if (err || !course) return res.status(404).send('Not found');
+    res.render('organiser-edit-course', { user: req.session.user, course });
+  });
+});
+
+// Handle edit
+router.post('/organiser/courses/:id/edit', ensureOrganiser, (req, res) => {
+  const { name, duration, description, location, price } = req.body;
+
+  coursesDB.update(
+    { _id: req.params.id },
+    { $set: { name, duration, description, location, price } },
+    {},
+    () => res.redirect('/organiser/dashboard')
+  );
+});
+
+// View participants
+router.get('/organiser/courses/:id/participants', ensureOrganiser, (req, res) => {
+  coursesDB.findOne({ _id: req.params.id }, (err, course) => {
+    if (err || !course) return res.status(404).send('Not found');
+    res.render('organiser-participants', { user: req.session.user, course });
   });
 });
 
